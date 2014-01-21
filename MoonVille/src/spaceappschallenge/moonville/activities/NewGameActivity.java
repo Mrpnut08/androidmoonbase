ecc71fc846edf851720669307f5aa85be8b150c2
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-21 12:13:27 -0500

    Remove previous code that has been rendered useless to the activity. New rewrite pending.

--- MoonVille/src/spaceappschallenge/moonville/activities/NewGameActivity.java
+++ MoonVille/src/spaceappschallenge/moonville/activities/NewGameActivity.java
@@ -7,18 +7,11 @@ import spaceappschallenge.moonville.R;
 import spaceappschallenge.moonville.domain.Difficulty;
 import spaceappschallenge.moonville.domain.GameDetails;
 import spaceappschallenge.moonville.factories.MoonBaseManager;
-import spaceappschallenge.moonville.factories.TransportCompanies;
-import spaceappschallenge.moonville.listadapters.TransportCompanySpinnerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 
 public class NewGameActivity extends GameActivity {
 	protected GameDetails gameDetails;
@@ -27,34 +20,14 @@ public class NewGameActivity extends GameActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_game);
-		
-		Spinner spinner = (Spinner) this.findViewById(R.id.choosespacetransport);
-		spinner.setAdapter( new TransportCompanySpinnerAdapter( TransportCompanies.getInstance().getCompanies() ) );
-		
+				
 	}
 
 	public void startNewGame(View view) {
 		gameDetails = GameDetails.getInstance();
-		retrieveUserInputs();
 		initModel();
 		showInitialLaunchScreen(view);
 		//showBaseOverviewScreen(view);
-	}
-
-	// Retrieve user name and difficulty level
-	protected void retrieveUserInputs() {
-		EditText userNameEditText = (EditText) findViewById(R.id.usercompanyname);
-		String userName = userNameEditText.getText().toString();
-		gameDetails.setUsername(userName);
-
-		RadioGroup difficultyRadioGroup = (RadioGroup) findViewById(R.id.choosedifficulty);
-		int radioButtonId = difficultyRadioGroup.getCheckedRadioButtonId();
-		RadioButton selectedButton = (RadioButton) difficultyRadioGroup
-				.findViewById(radioButtonId);
-		int difficulty = difficultyRadioGroup.indexOfChild(selectedButton);
-
-		gameDetails.setDifficultyLevel(difficulty);
-		Log.i("Difficulty:", "" + gameDetails.getDifficultyLevel());
 	}
 
 	protected void initModel() {
@@ -80,26 +53,4 @@ public class NewGameActivity extends GameActivity {
 				new Intent(this, BaseOverviewActivity.class));
 		this.finish();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_new_game, menu);
-		return true;
-	}
-	
-
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event)
-	{
-	    if ((keyCode == KeyEvent.KEYCODE_BACK))
-	    {
-			startActivity(
-					new Intent(this, MainMenuActivity.class));
-	        finish();
-	    }
-	    return super.onKeyDown(keyCode, event);
-	}
-
 }
