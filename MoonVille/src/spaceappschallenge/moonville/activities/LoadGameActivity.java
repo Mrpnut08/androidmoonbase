bc8ec8819514be0dae0f891f6670be95d42e69b2
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-22 21:09:55 -0500

    Add LoadgameActivity to the manifest. Correct some minor layout problems. Make the Create New file button invisible when Using LoadGame.

--- MoonVille/src/spaceappschallenge/moonville/activities/LoadGameActivity.java
+++ MoonVille/src/spaceappschallenge/moonville/activities/LoadGameActivity.java
@@ -1,6 +1,7 @@
 package spaceappschallenge.moonville.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoadGameActivity extends SaveFileManagerActivity {
 
@@ -8,6 +9,7 @@ public class LoadGameActivity extends SaveFileManagerActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.title.setText("Load Game");
-		this.new_file.setEnabled(false);;
+		this.new_file.setEnabled(false);
+		this.new_file.setVisibility(Button.INVISIBLE);
 	}
 }
