40a01eb0a242dc9d5d38afdae0aa86d628c7976c
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-25 11:26:32 -0500

    Add the functionality to create new save games.

--- MoonVille/src/spaceappschallenge/moonville/activities/SaveGameActivity.java
+++ MoonVille/src/spaceappschallenge/moonville/activities/SaveGameActivity.java
@@ -1,7 +1,11 @@
 package spaceappschallenge.moonville.activities;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
+import spaceappschallenge.moonville.domain.Difficulty;
+import spaceappschallenge.moonville.domain.GameDetails;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +14,16 @@ import android.widget.Toast;
 
 public class SaveGameActivity extends SaveFileManagerActivity implements OnClickListener{
 
+	private GameDetails metasave;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.title.setText("Save Game");
 		this.new_file.setOnClickListener(this);
+		
+		this.metasave = new GameDetails(this.getIntent().getStringExtra("playername"),
+										this.getIntent().getIntExtra("difficulty", Difficulty.DIF_MED));
 	}
 
 	@Override
@@ -22,10 +31,18 @@ public class SaveGameActivity extends SaveFileManagerActivity implements OnClick
 		
 		int filecount = this.file_chooser.getListSize();
 		
-		if (filecount < 5) {
-			File savedir = new File(this.getExternalFilesDir(null),"SaveSlot-"+filecount+File.separatorChar);
-			savedir.mkdirs();
+		if (filecount < 10) {
+			try {
+			File save_file = new File(this.getExternalFilesDir(null),"Save-"+filecount+".sav");
+			ObjectOutputStream output_stream = new ObjectOutputStream(new FileOutputStream(save_file));
+			output_stream.writeObject(this.metasave);
+			output_stream.close();
+			
 			this.startActivity(new Intent(this,NewGameActivity.class));
+			} catch (Exception err) {
+				
+			}
+			
 		} else {
 			Toast.makeText(this, "Max number of save files reached",Toast.LENGTH_LONG).show();
 		}
