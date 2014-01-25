5a9a61a13579d509e24b661915ae6535fa52b0e5
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-25 11:19:33 -0500

    Remove Singleton pattern from GameDetails and added move game turns into this class.

--- MoonVille/src/spaceappschallenge/moonville/domain/GameDetails.java
+++ MoonVille/src/spaceappschallenge/moonville/domain/GameDetails.java
@@ -10,18 +10,14 @@ public class GameDetails implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -7546492796233811239L;
-	String username = "";
-	int difficultyLevel = Difficulty.DIF_EASY;
-	private static GameDetails instance = null;
+	private String username;
+	private int difficulty_level;
+	private int turn;
 
-	protected GameDetails() {
-	}
-
-	public static GameDetails getInstance() {
-		if (GameDetails.instance == null) {
-			GameDetails.instance = new GameDetails();
-		}
-		return GameDetails.instance;
+	public GameDetails(String name, int difficulty) {
+		this.username = name;
+		this.difficulty_level = difficulty;
+		this.turn = 0;
 	}
 
 	public String getUsername() {
@@ -33,11 +29,14 @@ public class GameDetails implements Serializable{
 	}
 
 	public int getDifficultyLevel() {
-		return difficultyLevel;
+		return difficulty_level;
 	}
 
-	public void setDifficultyLevel(int difficultyLevel) {
-		this.difficultyLevel = difficultyLevel;
+	public void setDifficultyLevel(int difficulty_level) {
+		this.difficulty_level = difficulty_level;
 	}
-
+	
+	public int getTurn() {
+		return this.turn;
+	}
 }
