0dadfe0e1c5993f30289564a1fe6906d1d3e10a6
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-26 16:37:08 -0500

    Add savefile to the class to hold the save file name for ingame saving. modify the constructor to support savefile. Document class.

--- MoonVille/src/spaceappschallenge/moonville/domain/GameDetails.java
+++ MoonVille/src/spaceappschallenge/moonville/domain/GameDetails.java
@@ -3,40 +3,62 @@ package spaceappschallenge.moonville.domain;
 import java.io.Serializable;
 
 /**
- * Stores game specific settings (player name and difficulty).
+ * Stores game specific settings (player name, difficulty, current turn 
+ * and it's save file)
  */
 public class GameDetails implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -7546492796233811239L;
-	private String username;
-	private int difficulty_level;
-	private int turn;
+	
+	private static final long serialVersionUID = -7546492796233811240L;
+	
+	private String player_name; 
+	
+	private int difficulty_level; //< Holds one of the difficulty constants defined
+								  //< in the Difficulty class (EASY, MEDIUM or HARD).
+	
+	private int turn;	///< Represents the turn the player is currently at.
+	
+	private String save_file; ///< The name of the file the game is saved in.
+							  ///< This is not the full path.
 
-	public GameDetails(String name, int difficulty) {
-		this.username = name;
-		this.difficulty_level = difficulty;
+	///< Class constructor.
+	/**
+	 * Creates a new object based on information obtained through user input.
+	 * The generated object is assumed to be a new game, starting from 0.
+	 * @param player_name The player's name.
+	 * @param difficulty_level The player's chosen difficulty level.
+	 * @param save_file 
+	 */
+	public GameDetails(String player_name, int difficulty_level, String save_file) {
+		
+		//Stores the player's name and chosen difficulty.
+		this.player_name = player_name;
+		this.difficulty_level = difficulty_level;
+		
+		//Store the save file for future saves.
+		this.save_file = save_file;
+		
+		//Set the current turn to 0, meaning that the player has not
+		//yet started playing.
 		this.turn = 0;
 	}
 
-	public String getUsername() {
-		return username;
+	///< Gets the player's name.
+	public String getPlayerName() {
+		return player_name;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
+	///< Gets the difficulty level.
 	public int getDifficultyLevel() {
 		return difficulty_level;
 	}
-
-	public void setDifficultyLevel(int difficulty_level) {
-		this.difficulty_level = difficulty_level;
-	}
 	
+	///< Gets the current turn.
 	public int getTurn() {
 		return this.turn;
+	}
+	
+	///< Gets the save file's name.
+	public String getSaveFile(){
+		return this.save_file;
 	}
 }
