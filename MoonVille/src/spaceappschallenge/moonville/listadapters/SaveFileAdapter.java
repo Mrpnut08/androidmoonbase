5c58ee8f742cfe301ae5166abacb23bf9775b769
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-26 17:18:26 -0500

    Refactor adapter so that it filters out unreadable, incompatible, unrelated and/or corrupted files. The code is also sturdier than before.

--- MoonVille/src/spaceappschallenge/moonville/listadapters/SaveFileAdapter.java
+++ MoonVille/src/spaceappschallenge/moonville/listadapters/SaveFileAdapter.java
@@ -4,11 +4,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 import spaceappschallenge.moonville.R;
+import spaceappschallenge.moonville.domain.Difficulty;
 import spaceappschallenge.moonville.domain.GameDetails;
 import android.content.Context;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,38 +20,30 @@ import android.widget.TextView;
 
 public class SaveFileAdapter extends BaseAdapter {
 	
-	private String[] save_slots;
+	private ArrayList<GameDetails> saves; ///< Stores list of the game detail of usable saves that were found in the device.
+	
 	private Context context;
 	
 	public SaveFileAdapter(Context context) {
-		this.context = context;
-		if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-			this.save_slots = this.context.getExternalFilesDir(null).list(new FilenameFilter() {
-				
-				@Override
-				public boolean accept(File dir, String filename) {
-					return (filename.endsWith(".sav"));
-				}
-			});
-			
-			if(this.save_slots == null) {
-				this.save_slots = new String[0];
-			}
-		}	else {
-			this.save_slots = new String[0];
-		}
 		
+		//Store context for future updates.
+		this.context = context;
+		
+		//Initialize Arraylist.
+		this.saves = new ArrayList<GameDetails>();
+		
+		//update the save list.
+		this.updateList();
 	}
 
 	@Override
 	public int getCount() {
-		return this.save_slots.length;
+		return this.saves.size();
 	}
-
+	
 	@Override
 	public Object getItem(int position) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.saves.get(position);
 	}
 
 	@Override
@@ -59,33 +54,97 @@ public class SaveFileAdapter extends BaseAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		
-		try {
-		TextView number,name,difficulty,turn;
-		GameDetails save_meta;
+		//Get the corresponding GameDetails Object from the array.
+		GameDetails save = this.saves.get(position);
 		
-		File save_file = new File (parent.getContext().getExternalFilesDir(null),this.save_slots[position]);
-		ObjectInputStream input_stream = new ObjectInputStream(new FileInputStream(save_file));
-		save_meta = (GameDetails) input_stream.readObject();
-		input_stream.close();
-		
+		//Inflate the listitem_savefile layout.
 		LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-		View view = inflater.inflate(R.layout.listitem_savefile, parent, false);
+		convertView = inflater.inflate(R.layout.listitem_savefile, parent, false);
 		
-		number = (TextView) view.findViewById(R.id.lisf_number);
-		name = (TextView) view.findViewById(R.id.lisf_name);
-		difficulty = (TextView) view.findViewById(R.id.lisf_difficulty);
-		turn = (TextView) view.findViewById(R.id.lisf_turn);
+		//Extract the TextViews from the inflated layout.
+		TextView number,name,difficulty,turn;
+		number = (TextView) convertView.findViewById(R.id.lisf_number);
+		name = (TextView) convertView.findViewById(R.id.lisf_name);
+		difficulty = (TextView) convertView.findViewById(R.id.lisf_difficulty);
+		turn = (TextView) convertView.findViewById(R.id.lisf_turn);
 		
-		number.setText(String.valueOf(position));
-		name.setText(save_meta.getUsername());
-		difficulty.setText(String.valueOf(save_meta.getDifficultyLevel()));
-		turn.setText("Turn " + save_meta.getTurn());
-		
-		return view;
-		
-		} catch (Exception err){
-			return convertView;
+		//Translate the difficulty value to its name.
+		String difficulty_name = "";
+		switch(save.getDifficultyLevel()) {
+			case Difficulty.EASY:
+				difficulty_name = this.context.getString(R.string.difficulty_easy);
+				break;
+				
+			case Difficulty.NORMAL:
+				difficulty_name = this.context.getString(R.string.difficulty_normal);
+				break;
+				
+			case Difficulty.HARD:
+				difficulty_name = this.context.getString(R.string.difficulty_hard);
+				break;
 		}
+		
+		//Set the corresponding text to each view.
+		number.setText(String.valueOf(position));
+		name.setText(save.getPlayerName());
+		difficulty.setText(difficulty_name);
+		turn.setText("Turn " + save.getTurn());
+		
+		//return the finished view.
+		return convertView;
+		
 	}
 	
+	/**
+	 * Searches in the application external directory for savefiles and updates the 
+	 * adapter with it.
+	 */
+	private void updateList() {
+		
+		//Clear ArrayList.
+		this.saves.clear();
+		
+		//Return if Eternal media is not mounted.
+		if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+			this.notifyDataSetChanged();
+			return;
+		}
+		
+		//Get list of ".sav" files
+		FilenameFilter name_filter = new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String filename) {
+				return filename.endsWith(".sav");
+			}
+		}; // ~FilenameFilter name_filter = new FilenameFilter()
+		
+		String[] save_files = this.context.getExternalFilesDir(null).list(name_filter);
+				
+		//Obtain the GameDetail of each save file.
+		FileInputStream file_input;
+		ObjectInputStream save_input;
+		
+		for (String file : save_files) {
+			try {
+				//Open the file for reading.
+				file_input= new FileInputStream(new File (this.context.getExternalFilesDir(null),file));
+				save_input = new ObjectInputStream(file_input);
+				
+				//Read the First object (GameDetailObject) in the class)
+				this.saves.add((GameDetails) save_input.readObject());
+				
+				//Close the streams
+				save_input.close();
+				file_input.close();
+			
+			//Log the cause of failure in case of error.
+			} catch (Exception err) {
+				Log.d("save manager", "Error reading "+file+": ("+err.getClass().getSimpleName()+")"+err.getMessage());
+			}
+		}// ~for (String file : save_files)
+		
+		//Notify attached views to update with the new data.
+		this.notifyDataSetChanged();
+	}
 }
