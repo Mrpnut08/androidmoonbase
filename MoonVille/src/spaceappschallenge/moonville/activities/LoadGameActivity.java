c80ee8a877f5a59813cc9535315346d76d876300
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-27 16:36:51 -0500

    Add the rerouting so that the game send the player to the new player when an unstarted save is loaded.

--- MoonVille/src/spaceappschallenge/moonville/activities/LoadGameActivity.java
+++ MoonVille/src/spaceappschallenge/moonville/activities/LoadGameActivity.java
@@ -1,5 +1,6 @@
 package spaceappschallenge.moonville.activities;
 
+import spaceappschallenge.moonville.domain.GameDetails;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,9 +16,15 @@ public class LoadGameActivity extends SaveFileManagerActivity {
 	}
 
 	@Override
-	public void onSaveSelect(String filename) {
-		Intent intent = new Intent (this, BaseOverviewActivity.class);
-		intent.putExtra("savefile", filename);
+	public void onSaveSelect(GameDetails save) {
+		
+		Class<? extends GameActivity> activity;
+		activity= (save.getTurn() <0)? BaseOverviewActivity.class : NewGameActivity.class;
+		
+		Intent intent = new Intent (this, activity);
+		
+		intent.putExtra("savefile", save.getSaveFile());
+		
 		this.startActivity(intent);
 		this.finish();
 	}
