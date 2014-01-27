8213432f03e19ed94b2d4d42c8ef6391949a3af0
Alfredo Giscombe <mrpnut08@gmail.com>
2014-01-27 17:18:00 -0500

    Clean up the NewGameActivity since its new role is that of a simple message Activity and data carrier.

--- MoonVille/src/spaceappschallenge/moonville/activities/NewGameActivity.java
+++ MoonVille/src/spaceappschallenge/moonville/activities/NewGameActivity.java
@@ -4,9 +4,7 @@
 package spaceappschallenge.moonville.activities;
 
 import spaceappschallenge.moonville.R;
-import spaceappschallenge.moonville.domain.Difficulty;
 import spaceappschallenge.moonville.domain.GameDetails;
-import spaceappschallenge.moonville.factories.MoonBaseManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,32 +20,11 @@ public class NewGameActivity extends GameActivity {
 	}
 
 	public void startNewGame(View view) {
-		initModel();
-		showInitialLaunchScreen(view);
-		//showBaseOverviewScreen(view);
-	}
-
-	protected void initModel() {
-		// Dummy models
-
-		Difficulty diff = new Difficulty(gameDetails.getDifficultyLevel());
-
-		MoonBaseManager.createNewMoonBase(diff, this);
-		// MoonBase should become a singleton I think, best way to make it
-		// easily accessible -Jos
-	}
-
-	public void showInitialLaunchScreen(View view){
-		view.getContext().startActivity(new Intent(this,InitialLaunchActivity.class));
-		this.finish();
-	}
-	// I changed the init method to this one, which is triggered by the onClick
-	// property in the xml of the button
-	// since we are using separate activities, this is a very easy way to
-	// implement navigation IMO.
-	public void showBaseOverviewScreen(View view) {
-		view.getContext().startActivity(
-				new Intent(this, BaseOverviewActivity.class));
+		
+		Intent intent = new Intent(this.getIntent());
+		intent.setClass(this, InitialLaunchActivity.class);
+		
+		this.startActivity(intent);	
 		this.finish();
 	}
 }
